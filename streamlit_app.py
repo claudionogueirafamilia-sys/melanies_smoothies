bad60320ed46c0c07099899acc935481fcf31d00
claudionogueirafamilia-sys <claudionogueirafamilia@gmail.com>
2026-01-15 15:58:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,5 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(Data=smoothiefroot_response.jason(), use_container_width=True)
 
